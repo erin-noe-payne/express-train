6e03c3808ba8ebfa81e017ba94b3efff682a67f3
Erin Noe-Payne <saizul@gmail.com>
2012-11-27 11:07:22 -0500

    cleaning out

--- scripts/generators/templates/lib/mongoose.js
+++
@@ -1,9 +0,0 @@
-// Connect to mongodb
-
-var mongoose = require('mongoose');
-
-module.exports = function(app) {
-  mongoose.connect('mongodb://' + app.config.mongoose.host + '/' + app.config.mongoose.db, function(err) {
-    if (err) throw new Error(err.message);
-  });
-};
---
+++ scripts/new.js
@@ -0,0 +1,7 @@
+/**
+ * Created by JetBrains WebStorm.
+ * User: erinnp
+ * Date: 11/26/12
+ * Time: 8:01 PM
+ * To change this template use File | Settings | File Templates.
+ */
