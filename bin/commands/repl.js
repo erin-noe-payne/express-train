200581dfd9b4d95cfc39d82a189690f117b49880
Erin Noe-Payne <saizul@gmail.com>
2012-12-03 00:27:26 -0500

    added repl functionality

---
+++ bin/commands/repl.js
@@ -0,0 +1,21 @@
+var repl = require('repl'),
+    path = require('path'),
+    train = require('../../lib/app');
+
+module.exports = function(program) {
+    var cmd = program.command('repl');
+
+    cmd.action(function(){
+        var appPath = path.join(process.cwd(), '/app');
+        var app = train(appPath);
+
+        var context = repl.start({
+            prompt: app.constants.name+'> ',
+            input: process.stdin,
+            output: process.stdout
+        }).context;
+
+        context.app = app;
+    });
+
+}
