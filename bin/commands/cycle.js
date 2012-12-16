4f6a69c6588c95ba38cd30a960dd4b7dc411a29e
Erin Noe-Payne <saizul@gmail.com>
2012-12-16 12:34:15 -0500

    fixed cycle

--- bin/commands/cycle.js
+++ bin/commands/cycle.js
@@ -1,40 +1,28 @@
 var path = require('path'),
     fs = require('fs'),
-    ps = require('child_process');
+    ps = require('child_process'),
+    spawn = require('child_process').spawn
 _ = require('underscore');
 
 module.exports = function (program) {
     var cmd = program.command('cycle')
-        .usage('runs the express train application with a watch on all files to restart on change for easier development')
+        .usage('runs the express train application using nodemon to restart on file change for easier development')
         .option('-d, --debug', 'Node process will listen on debug port');
 
-    //capture arbitrary options and pass them on
     cmd.action(function () {
         var appPath = path.join(process.cwd(), '/app/index.js');
         var runnerPath = path.join('./node_modules/express-train/lib/runner')
-        var app;
 
-        run();
-
-        function run() {
-            console.log('  \033[37mstarting application...\033[0m');
-            app = ps.fork(runnerPath, [appPath, cmd.debug ? '--debug' : ''], {cwd:process.cwd()});
-
-            app.once('exit', run);
+        var args = [runnerPath, appPath];
+        if(cmd.debug) {
+            args.unshift('--debug');
         }
 
+        var nodemon = spawn('./node_modules/express-train/node_modules/.bin/nodemon', args, {
+            cwd:process.cwd()
+        });
 
-        function watch(dir, cb) {
-
-        }
-        //TODO: watch fires only once.
-        fs.watch(path.join('.', 'package.json'), kill);
-
-        function kill(){
-            console.log('\n');
-            console.log('  \033[37file change detected, killing application...\033[0m');
-            app.kill('SIGKILL');
-        }
-
+        nodemon.stdout.pipe(process.stdout);
+        nodemon.stderr.pipe(process.stderr);
     });
 }
