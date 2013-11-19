9eee4a1ea8635e29a2ec0b0775b52618e093b414
erin.noe-payne <erin.noe-payne@logrhythm.com>
2013-11-19 14:18:32 -0700

    Update to nject 1.0.0 to support async dependencies

--- bin/commands/console.js
+++ bin/commands/console.js
@@ -5,31 +5,32 @@ var repl = require('repl'),
     resolveProjectRoot = require('../../lib/resolveProjectRoot'),
     history = require('repl.history');
 
-module.exports = function(program) {
-    var cmd = program.command('console')
-        .usage('console [file]')
-        .description('Opens an interactive node console with the your express train application loaded. Each registered ' +
-        'dependency is available globally on the command line. Defaults to loading app/index.js, unless a ' +
-        'main file is specified');
+module.exports = function (program) {
+  var cmd = program.command('console')
+      .usage('console [file]')
+      .description('Opens an interactive node console with the your express train application loaded. Each registered ' +
+          'dependency is available globally on the command line. Defaults to loading app/index.js, unless a ' +
+          'main file is specified');
 
-    //TODO: implement sandbox?
-    cmd.action(function(){
-        var fileArg = process.argv[3]
+  cmd.action(function () {
+    var fileArg = process.argv[3]
 
-        var app = require(resolveProjectRoot(fileArg));
-        var historyPath = path.join(__dirname, '../../.console_history');
+    var historyPath = path.join(__dirname, '../../.console_history');
 
-        var r = repl.start({
-            prompt: '> ',
-            input: process.stdin,
-            output: process.stdout
-        });
-
-        history(r, historyPath);
-
-        r.on('exit', process.exit);
-
-        _.extend(r.context, app);
+    var r = repl.start({
+      prompt: '> ',
+      input: process.stdin,
+      output: process.stdout
     });
+
+    history(r, historyPath);
+
+    r.on('exit', process.exit);
+
+    //TODO: this will be broken by async resolution
+    var app = require(resolveProjectRoot(fileArg));
+
+    _.extend(r.context, app);
+  });
 
 }
--- test/app.js
+++ test/app.js
@@ -11,147 +11,151 @@ var MODULE_TEMPLATE = 'module.exports = function(){}';
 
 describe('express-train', function () {
 
-    var dirStructure = {
-        config: {
-            'default.json': '{"name":"max"}',
-            'production.json': '{"name":"bart"}',
-            'handlebars.json': '{"name":"{{NAME}}"}'
+  var dirStructure = {
+    config: {
+      'default.json': '{"name":"max"}',
+      'production.json': '{"name":"bart"}',
+      'handlebars.json': '{"name":"{{NAME}}"}'
+    },
+    app: {
+      controllers: {
+        '.hiddenFile': 1,
+        '.hiddenDirectory': {
+          burried: 1,
+          content: 1
         },
-        app: {
-            controllers: {
-                '.hiddenFile':1,
-                '.hiddenDirectory': {
-                    burried:1,
-                    content:1
-                },
-                ApiCtrl:1,
-                ViewCtrl:1
-            },
-            lib: {},
-            middleware: {},
-            models: {
-                Users:1,
-                Accounts:1
-            },
-            public: {},
-            views: {},
-            'configOverride.json':'{"name":"william"}'
-        }
+        ApiCtrl: 1,
+        ViewCtrl: 1
+      },
+      lib: {},
+      middleware: {},
+      models: {
+        Users: 1,
+        Accounts: 1
+      },
+      public: {},
+      views: {},
+      'configOverride.json': '{"name":"william"}'
     }
+  }
 
-    var app;
-    beforeEach(function(){
-        hydrate(dirStructure, BASE_DIR)
+  var app;
+  beforeEach(function () {
+    hydrate(dirStructure, BASE_DIR)
+  })
+
+  afterEach(function () {
+    cleanup(BASE_DIR)
+    process.removeAllListeners('uncaughtException')
+    delete process.env.NODE_ENV
+    delete app;
+  })
+
+  describe('app', function () {
+    it('returns an object', function () {
+      train(APP_DIR, function (err, app) {
+        app.should.be.an.instanceOf(Object)
+      })
     })
-
-    afterEach(function(){
-        cleanup(BASE_DIR)
-        process.removeAllListeners('uncaughtException')
-        delete process.env.NODE_ENV
-        delete app;
+    it('provides a the config constant', function () {
+      app = train(APP_DIR, function (err, app) {
+        app.config.should.be.an.instanceOf(Object)
+      })
     })
-
-    describe('app', function(){
-        it('returns an object', function(){
-            app = train(APP_DIR)
-            app.should.be.an.instanceOf(Object)
-        })
-        it('provides a the config constant', function(){
-            app = train(APP_DIR)
-            app.config.should.be.an.instanceOf(Object)
-        })
-        it('provides an express app via `app`', function(){
-            app = train(APP_DIR)
-            should.exist(app.app)
-            app.app.listen.should.be.an.instanceOf(Function)
-        })
-        it('allows override of app if an app.js file is present', function(){
-            var localDirStructure = _.cloneDeep(dirStructure),
-                appFile = "module.exports = function(){return 'hello world'}";
-
-            localDirStructure.app.lib.app = appFile;
-
-            cleanup(BASE_DIR);
-            hydrate(localDirStructure, BASE_DIR);
-
-            app = train(APP_DIR);
-
-            app.app.should.equal('hello world');
-        })
-        it('ignores files that begin with `.`', function(){
-            app = train(APP_DIR)
-            should.not.exist(app['.hiddenFile'])
-        })
-        it('ignores directories that begin with `.`', function(){
-            app = train(APP_DIR)
-            should.not.exist(app.burried)
-            should.not.exist(app.content)
-        })
-        it('respects the locations override', function(){
-            app = train(APP_DIR, {config: {path: 'configOverride.json'}})
-
-            app.config.name.should.equal('william')
-        })
-
+    it('provides an express app via `app`', function () {
+      app = train(APP_DIR, function (err, app) {
+        should.exist(app.app)
+        app.app.listen.should.be.an.instanceOf(Function)
+      })
     })
+    it('allows override of app if an app.js file is present', function () {
+      var localDirStructure = _.cloneDeep(dirStructure),
+          appFile = "module.exports = function(){return 'hello world'}";
 
-    describe('config', function(){
-        it('if node env is not set, defaults to default.json', function(){
-            process.env.NODE_ENV = undefined;
-            app = train(APP_DIR)
-            app.config.name.should.equal('max')
-        })
-        it('loads based on NODE_ENV', function(){
-            process.env.NODE_ENV = 'production';
-            app = train(APP_DIR)
-            app.config.name.should.equal('bart')
-        })
-        it('if NODE_ENV does not match a config, defaults to default.json', function(){
-            process.env.NODE_ENV = 'test';
-            app = train(APP_DIR)
-            app.config.name.should.equal('max')
-        })
-        it('is handlebars compiled against env variables', function (){
-            process.env.NODE_ENV = 'handlebars';
-            process.env.NAME = 'ted';
-            app = train(APP_DIR)
-            app.config.name.should.equal('ted')
-        })
+      localDirStructure.app.lib.app = appFile;
+
+      cleanup(BASE_DIR);
+      hydrate(localDirStructure, BASE_DIR);
+
+      app = train(APP_DIR, function (err, app) {
+        app.app.should.equal('hello world');
+      });
     })
+    it('ignores files that begin with `.`', function () {
+      app = train(APP_DIR, function (err, app) {
+        should.not.exist(app['.hiddenFile'])
+      })
+    })
+    it('ignores directories that begin with `.`', function () {
+      app = train(APP_DIR, function (err, app) {
+        should.not.exist(app.burried)
+        should.not.exist(app.content)
+      })
+    })
+    it('respects the locations override', function () {
+      app = train(APP_DIR, {config: {path: 'configOverride.json'}}, function (err, app) {
+        app.config.name.should.equal('william')
+      })
+    })
+  })
 
-
-
-
+  describe('config', function () {
+    it('if node env is not set, defaults to default.json', function () {
+      process.env.NODE_ENV = undefined;
+      app = train(APP_DIR, function (err, app) {
+        app.config.name.should.equal('max')
+      })
+    })
+    it('loads based on NODE_ENV', function () {
+      process.env.NODE_ENV = 'production';
+      app = train(APP_DIR, function (err, app) {
+        app.config.name.should.equal('bart')
+      })
+    })
+    it('if NODE_ENV does not match a config, defaults to default.json', function () {
+      process.env.NODE_ENV = 'test';
+      app = train(APP_DIR, function (err, app) {
+        app.config.name.should.equal('max')
+      })
+    })
+    it('is handlebars compiled against env variables', function () {
+      process.env.NODE_ENV = 'handlebars';
+      process.env.NAME = 'ted';
+      app = train(APP_DIR, function (err, app) {
+        app.config.name.should.equal('ted')
+      })
+    })
+  })
 });
 
 function hydrate(structure, location) {
-    fs.mkdirSync(location)
-    _.each(structure, function (definition, name) {
-        var nextLocation = path.join(location, name)
-        if (_.isObject(definition)) {
-            hydrate(definition, nextLocation);
-        } else {
-            var template = MODULE_TEMPLATE
-            if (_.isString(definition)) {
-                template = definition
-            }
-            fs.writeFileSync(nextLocation, template)
-        }
-    });
+  fs.mkdirSync(location)
+  _.each(structure, function (definition, name) {
+    var nextLocation = path.join(location, name)
+    if (_.isObject(definition)) {
+      hydrate(definition, nextLocation);
+    } else {
+      var template = MODULE_TEMPLATE
+      if (_.isString(definition)) {
+        template = definition
+      }
+      fs.writeFileSync(nextLocation, template)
+    }
+  });
 }
 
 function cleanup(dir) {
-    var files = [];
-    if (fs.existsSync(dir)) {
-        files = fs.readdirSync(dir);
-        files.forEach(function (file, index) {
-            var curPath = dir + "/" + file;
-            if (fs.statSync(curPath).isDirectory()) { // recurse
-                cleanup(curPath);
-            } else { // delete file
-                fs.unlinkSync(curPath);
-            }
-        });
-        fs.rmdirSync(dir);
-    }
+  var files = [];
+  if (fs.existsSync(dir)) {
+    files = fs.readdirSync(dir);
+    files.forEach(function (file, index) {
+      var curPath = dir + "/" + file;
+      if (fs.statSync(curPath).isDirectory()) { // recurse
+        cleanup(curPath);
+      } else { // delete file
+        fs.unlinkSync(curPath);
+      }
+    });
+    fs.rmdirSync(dir);
+  }
 };
